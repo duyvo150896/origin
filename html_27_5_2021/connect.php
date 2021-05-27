cfe57ff08803c96bea93c44a0cea7952c5509720
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2021-05-28 00:22:09 +0700

    Revert "rm web"
    This reverts commit a0160bb9c55050e4e96b1fcd516146c494904185.

---
+++ html_27_5_2021/connect.php
@@ -0,0 +1,5 @@
+<?php
+$db = mysqli_connect ('localhost','root','Pass1234','paysys');
+mysqli_set_charset($db, 'utf8');
+
+?>
---
+++ html_27_5_2021/napthe/user_process.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+define('IN_ECS', true);
+// include file connect
+include 'inc/#config.php';
+// Make connect
+$dbconn = new connectMySQL;
+// Connect to acc_db
+$dbconn->connect('paysys');
+
+	if($_GET['page']=='dangxuat'){
+		$_SESSION['user_login'] = '';
+		session_destroy();
+		echo "Thoát thành công!";
+	}
+?>
