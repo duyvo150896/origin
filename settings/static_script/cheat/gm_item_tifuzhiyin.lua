dc459ec3328921ed9ea967a3c62758b1bac06e22
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2021-08-08 00:49:28 +0700

    Update gm_item_tifuzhiyin.lua

--- settings/static_script/cheat/gm_item_tifuzhiyin.lua
+++ settings/static_script/cheat/gm_item_tifuzhiyin.lua
@@ -135,7 +135,7 @@ function tieudung_orther()
 			-- "Thay ®æi h­íng Phôc Sinh/change_PhucSinh",
 			-- "Sinh ThÇn Cang gäi Boss bang héi/sinhthancang",
             -- "M¶nh b¹ch kim/nhanmbk",
-            "NhËn danh hiÖu/Get_Danh_Hieu2",
+            -- "NhËn danh hiÖu/Get_Danh_Hieu2",
             "T¨ng ®iÓm nh©n vËt/Get_Diem_Char",
             -- "NhËn C¸c Lo¹i Nguyªn LiÖu/Get_Nguyen_Lieu",
 			"Thao t¸c bang héi/TongOperation",
