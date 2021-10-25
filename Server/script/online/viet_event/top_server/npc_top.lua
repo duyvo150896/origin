58aa4480689b1c88c7ee0d43b48af9c0eab17651
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2021-10-25 19:32:53 +0700

    Update npc_top.lua

--- Server/script/online/viet_event/top_server/npc_top.lua
+++ Server/script/online/viet_event/top_server/npc_top.lua
@@ -291,7 +291,7 @@ function confirm_get_top_faction_award()
 	local nNation = GetGlbValue(GLB_TSK_SERVER_ID)
 	local nNationality = GetByte(GetTask(TSK_SERVER_ID), TSK_SERVER_ID_CUR)
 	local nChuyenSinh = gf_GetTaskByte(TRANSLIFE_TASK_ID, TRANSLIFE_BYTE_COUNT) + GetPlayerRebornParam(0)
-	local tbRoute = {[2]=1,[3]=1,[4]=1,[6]=1,[8]=1,[9]=1,[11]=1,[12]=1,[14]=1,[15]=1,[17]=1,[18]=1,[20]=1,[21]=1,[23]=1,[29]=1,[30]=1}
+	local tbRoute = {[2]=1,[3]=1,[4]=1,[6]=1,[8]=1,[9]=1,[11]=1,[12]=1,[14]=1,[15]=1,[17]=1,[18]=1,[20]=1,[21]=1,[23]=1,[25]=1,[26]=1,[27]=1,[29]=1,[30]=1}
 	if nNationality ~= nNation then
 		Talk(1, "", strNpcName .. "C¸c h¹ kh«ng ph¶i lµ thÇn d©n bæn quèc nªn kh«ng thÓ nhËn th­ëng!")
 		return 0
