c9d7279851427b2d567c3b4696982bfe8406dd47
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2021-10-31 18:03:02 +0700

    Update hocskill85.lua

--- Server/script/owner/hocskill85.lua
+++ Server/script/owner/hocskill85.lua
@@ -8,6 +8,11 @@ function HocSkill85()
 	local CapDo = 85;--level
 	local MonPhai = GetPlayerRoute();--lÊy th«ng tin m«n ph¸i
 	
+	if kiem_tra() > 0 then
+	Say("§¹i hiÖp ®· häc 5 tÇng trÊn ph¸i råi kh«ng lo ®i luyÖn tËp ®i./nothing")
+	return
+	end
+	
 	if  GetLevel() < CapDo then
 		Talk(1, "", strNpcName.."§¼ng cÊp cña c¸c h¹ ch­a ®ñ "..CapDo..", h·y cè g¾ng thªm..");
 		return 0
@@ -35,12 +40,22 @@ function HocSkill85()
 
 	if GetVipJinJuan()  >= 3000 then
 		ModifyJinJuan(-3000,1)
-		enter_mp(MonPhai)
+		enter_mp85(MonPhai)
+		SetTask(3500,1)
 	end
 
 end
 
-function enter_mp(nRoute)
+function kiem_tra()
+
+	if GetTask(3500) == 1 then
+		return 1;
+	else
+		return 0;
+	end
+end
+
+function enter_mp85(nRoute)
 	local nBegin = 0;
 	local nEnd = 0;
 	local nBody = GetBody(); --1=Õý³£ÄÐ£¬2=¿ýÎµÄÐ£¬3=ÐÔ¸ÐÅ®£¬4=½¿Ð¡Å®
@@ -48,128 +63,102 @@ function enter_mp(nRoute)
 	--SetPlayerRoute(nRoute);						--c¸i nµy ®Ó cho nã v« m«n ph¸i nÕu nã ch­a v«
 
 	if nRoute == 2 then						--ÉÙÁÖË×¼Ò
-		LearnSkill(3);
-		LearnSkill(5);
-		LearnSkill(31);
+		LearnSkill(32);
 		nBegin = 21;
 		nEnd = 31;
 	elseif nRoute == 3 then					--ÉÙÁÖìøÉ®
-		LearnSkill(6);
-		LearnSkill(56);
+		LearnSkill(57);
 		nBegin = 45;
 		nEnd = 56;
 	elseif nRoute == 4 then					--ÉÙÁÖÎäÉ®
-		LearnSkill(2);
-		LearnSkill(43);
+		LearnSkill(44);
 		nBegin = 33;
 		nEnd = 43;
 	elseif nRoute == 14 then					--Îäµ±µÀ¼Ò
-		LearnSkill(4);
-		LearnSkill(145);
+		LearnSkill(146);
 		nBegin = 125;
 		nEnd = 145;
 	elseif nRoute == 15 then					--Îäµ±Ë×¼Ò
-		LearnSkill(5);
-		LearnSkill(158);
+		LearnSkill(159);
 		nBegin = 147;
 		nEnd = 158;
 	elseif nRoute == 8 then					--¶ëáÒ·ð¼Ò
-		LearnSkill(4);
-		LearnSkill(88);
+		LearnSkill(89);
 		nBegin = 75
 		nEnd = 88;
 	elseif nRoute == 9 then					--¶ëáÒË×¼Ò
-		LearnSkill(10);
-		LearnSkill(101);
+		LearnSkill(102);
 		nBegin = 90
 		nEnd = 101;
 	elseif nRoute == 11 then					--Ø¤°ï¾»Ò¢
-		LearnSkill(2);
-		LearnSkill(112);
+		LearnSkill(113);
 		nBegin = 103
 		nEnd = 112;
 	elseif nRoute == 12 then					--Ø¤°ïÎÛÒ¢
-		LearnSkill(5);
-		LearnSkill(123);
+		LearnSkill(124);
 		nBegin = 114
 		nEnd = 123;
 	elseif nRoute == 31 then					--ÉÙÁÖÎäÉ®
-		LearnSkill(17);
-		LearnSkill(1882);
+		LearnSkill(1883);
 		nBegin = 1872;
 		nEnd = 1882;
 	elseif nRoute == 6 then					--ÌÆÃÅ
-		LearnSkill(7);
-		LearnSkill(73);
+		LearnSkill(74);
 		nBegin = 58
 		nEnd = 73;
 	elseif nRoute == 17 then					--ÑîÃÅÇ¹Æï
-		LearnSkill(11);
-		LearnSkill(731);
+		LearnSkill(732);
 		nBegin = 720;
 		nEnd = 731;
 	elseif nRoute == 18 then					--ÑîÃÅ¹­Æï
-		LearnSkill(12);
-		LearnSkill(744);
+		LearnSkill(745);
 		nBegin = 733;
 		nEnd = 744;
 	elseif nRoute == 20 then					--Îå¶¾Ð°ÏÀ
-		LearnSkill(13);
-		LearnSkill(774);
+		LearnSkill(775);
 		nBegin = 364;
 		nEnd = 377;
 	elseif nRoute == 21 then					--Îå¶¾¹ÆÊ¦
-		LearnSkill(14);
-		LearnSkill(773);
+		LearnSkill(774);
 		nBegin = 347;
 		nEnd = 363;
 	elseif nRoute == 32 then					--À¥¢ØÌìÊ¦
-		LearnSkill(1885);
-		LearnSkill(1898);
+		LearnSkill(1899);
 		nBegin = 1886 ;
 		nEnd = 1898;
 	elseif nRoute == 23 then					--À¥¢ØÌìÊ¦
-		LearnSkill(4);
-		LearnSkill(1031);
+		LearnSkill(1032);
 		nBegin = 1017 ;
 		nEnd = 1031;
 	elseif nRoute == 25 then					--Ã÷½ÌÊ¥Õ½
-		LearnSkill(3);
-		LearnSkill(1065);
+		LearnSkill(1066);
 		nBegin = 1053 ;
 		nEnd = 1065;
 	elseif nRoute == 26 then					--Ã÷½ÌÕó±ø
-		LearnSkill(8);
-		LearnSkill(1095);
+		LearnSkill(1096);
 		nBegin = 1083 ;
 		nEnd = 1095;
 	elseif nRoute == 27 then					--Ã÷½ÌÑªÈË
-		LearnSkill(14);
-		LearnSkill(1212);
+		LearnSkill(1213);
 		nBegin = 1131 ;
 		nEnd = 1143;
 	elseif nRoute == 29 then					--´äÑÌÎèÏÉ
-		LearnSkill(15);
-		LearnSkill(1195);
+		LearnSkill(1196);
 		nBegin = 1165 ;
 		nEnd = 1176;
 	elseif nRoute == 30 then					--´äÑÌÁéÅ®
-		LearnSkill(16);
-		LearnSkill(1229);
+		LearnSkill(1230);
 		nBegin = 1217 ;
 		nEnd = 1229;
 	else
 		return
 	end;
 
-	LearnSkill(20);
-
 	for i = nBegin, nEnd do
 		LearnSkill(i);
-		while LevelUpSkill(i) == 1 do
+		local sotanghientai= GetskillLevel(i)
+		X = 5 - sotanghientai
+		for (j = 0; j < X ; i++)
+		LevelUpSkill(i)
 		end
 	end;
-
-	SaveNow()
-	Talk(1,"ExitGame","Thao t¸c thµnh c«ng, h·y ®¨ng nhËp l¹i")
-end
