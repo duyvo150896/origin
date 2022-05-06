84addbec248d12bcabbb424500c5a4ef2b133593
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2022-05-06 23:53:07 +0700

    Update lanhua_bag.lua

--- script/online/viet_event/lanhua_boss/lanhua_bag.lua
+++ script/online/viet_event/lanhua_boss/lanhua_bag.lua
@@ -18,23 +18,33 @@ g_nNeedWeight = 20
 g_nDenominator = 100							--·ÖÄ¸£¬±íÊ¾¸ÅÂÊµÄµ¥Î»ÊÇx·ÖÖ®1
 
 g_szFileName = "lanhua_bag.lua"					--ÎÄ¼þÃû
-
+--30001 - 30012
 g_tbItem = 
 {
 	--		ÀàÐÍ	   ¸ÅÂÊ	ÊýÁ¿ Ãû×Ö	ID1,ID2,ID3
-	[1] = {TYPE_EQUIP,	50,	1,	"§«ng Ph­¬ng Long Ch©u",	0,	102,	24,	},
-	[2] = {TYPE_EQUIP,	25,	1,	"Nh­ ý",		0,	102,	23,	},
-	[3] = {TYPE_EQUIP,	25,	1,	"C¸t T­êng",		0,	102,	22,	},
+	[1] = {TYPE_ITEM,	3,	1,	"NguyÖt Hoa Chi Tinh",	2,	1,	3219,},
+	[2] = {TYPE_EQUIP,	3,	1,	"§«ng Ph­¬ng Long Ch©u",	0,	102,	24,},
+	[3] = {TYPE_EQUIP,	6,	1,	"Tµng kiÕm ngäc",0,	102,30001,},
+	[4] = {TYPE_EQUIP,	8,	1,	"Cöu thiªn ngäc",	0,	102,	129,},
+	[5] = {TYPE_ITEM,	20,	50,	"Bao Linh Th¹ch 7",	2,	1,	30419,},
+	[6] = {TYPE_ITEM,	60,	50,	"Bao Linh Th¹ch 6",	2,	1,	30418,},
+
+
 }
 
 
 
 --=========================================================================================
 function OnUse(nItemIdx)
+	local tmp=1;
 	if gf_JudgeRoomWeight(g_nNeedRoom,g_nNeedWeight) == 0 then
 		Talk(1,"","Kho¶ng trèng hµnh trang hoÆc søc lùc kh«ng ®ñ, xin kiÓm tra l¹i!")
 		return
 	end
+	-- if tmp == 1 then
+		-- Talk(1,"","T¹m thêi kh«ng thÓ sö dông, H·y tÝch lòy, Admin sÏ th«ng b¸o sau!")
+		-- return
+	-- end
 	if DelItemByIndex(nItemIdx,1) == 1 then
 		use_it()
 	end
@@ -43,6 +53,17 @@ end
 function use_it()
 	--ÎïÆ·½±Àø²¿·Ö
 	local nRandIdx = get_random_item(g_tbItem)
+	local random_id_tangkiem=random(0,11)
+	local random_id_9t=random(0,6)
+	local lv_rand = random(1,2)
+	local lv = 1
+	
+	if lv_rand == 1 then
+		lv = 13030
+	elseif lv_rand == 2 then
+		lv = 794
+	end
+	
 	if nRandIdx == 0 then
 		return
 	end
@@ -50,10 +71,21 @@ function use_it()
 	local szItemName = g_tbItem[nRandIdx][4]
 	local nItemNum = g_tbItem[nRandIdx][3]
 	local szInfoDescribe = format("(%d,%d,%d)", g_tbItem[nRandIdx][5], g_tbItem[nRandIdx][6], g_tbItem[nRandIdx][7])
-
+	local Name = GetName()
+	local msg = format("Ng­êi ch¬i "..Name.." sö dông tói hµnh trang nhËn ®­îc §«ng Ph­¬ng Long Ch©u, thËt may m¾n  !!");
+	-- gf_EventGiveRandAward(tAward, gf_SumRandBase(tAward), 1, "BOSS ThÕ giíi", "§¹i ChiÕn B¶o H¹p")
 	local nRetCode = 0
-	nRetCode = AddItem(g_tbItem[nRandIdx][5],g_tbItem[nRandIdx][6],g_tbItem[nRandIdx][7],g_tbItem[nRandIdx][3],1,-1,-1,-1,-1,-1,-1)
-
+	if nRandIdx == 2 then
+		nRetCode = AddItem(g_tbItem[nRandIdx][5],g_tbItem[nRandIdx][6],g_tbItem[nRandIdx][7],g_tbItem[nRandIdx][3],1,1,lv,-1,-1,-1,-1)
+		Msg2Global(msg);
+		AddLocalNews(msg);
+	elseif nRandIdx == 3  then
+		nRetCode = AddItem(g_tbItem[nRandIdx][5],g_tbItem[nRandIdx][6],g_tbItem[nRandIdx][7] + random_id_tangkiem,g_tbItem[nRandIdx][3],1,-1,-1,-1,-1,-1,-1)
+	elseif nRandIdx == 4  then
+		nRetCode = AddItem(g_tbItem[nRandIdx][5],g_tbItem[nRandIdx][6],g_tbItem[nRandIdx][7] + random_id_9t,				g_tbItem[nRandIdx][3],1,-1,-1,-1,-1,-1,-1)
+	else
+		nRetCode = AddItem(g_tbItem[nRandIdx][5],g_tbItem[nRandIdx][6],g_tbItem[nRandIdx][7] ,g_tbItem[nRandIdx][3])
+	end
 	if nRetCode == 1 then
 		Msg2Player("B¹n nhËn ®­îc  "..nItemNum.." "..szItemName)
 		WriteLogEx("Hoat dong thang 6", "NhËn ®­îc", nItemNum, szItemName, szInfoDescribe, GetTongName() )
@@ -65,10 +97,12 @@ end
 --°´ÕÕ¸ÅÂÊ»ñµÃg_tbItemÀïÃæµÄÒ»ÏîÄÚÈÝ
 function get_random_item(nTab)
 	local nRandom = random(1,g_nDenominator)
+	Msg2Global("nRandom:"..nRandom)
 	local nBase = 0
 	if check_item_tab(nTab) == 1 then
 		for i=1,getn(nTab) do
 			if nRandom <= nTab[i][2]+nBase then
+				Msg2Global("i:"..i)
 				return i
 			else
 				nBase = nBase + nTab[i][2]
