7602b293efa7af48aa7fa82c4260adf4d6d24013
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2022-05-06 23:59:35 +0700

    Update wb_head.lua

--- script/function/world_boss/wb_head.lua
+++ script/function/world_boss/wb_head.lua
@@ -7,6 +7,10 @@ Include("\\script\\online_activites\\award.lua")
 
 function wb_RelayCallCreateBoss(nRandSeed)
 	--ÊÇ·ñ¹Ø±Õ
+	local ret =1
+	if ret == 1 then
+		return
+	end
 	if not WB_SWITCH or WB_SWITCH ~= 1 then
 		return
 	end
@@ -22,7 +26,7 @@ function wb_RelayCallCreateBoss(nRandSeed)
 	randomseed(nRandSeed)
 	local nIndex = random(getn(WB_WORLD_BOSS));
 	--¹ã²¥
-	local msg = format("Theo t×nh b¸o, %s hiÖn ®ang xuÊt hiÖn ë b¶n ®å %s, c¸c hiÖp sÜ h·y ®Õn ®ã xem thö.", WB_WORLD_BOSS[nIndex][2], WB_WORLD_BOSS_MAP[nIndex]);
+	local msg = format("Theo t×nh b¸o, %s hiÖn ®ang xuÊt hiÖn ?b¶n ®å %s, c¸c hiÖp s?h·y ®Õn ®ã xem th?", WB_WORLD_BOSS[nIndex][2], WB_WORLD_BOSS_MAP[nIndex]);
 	Msg2Global(msg);
 	AddLocalNews(msg);
 	Msg2Global(WB_WORLD_BOSS_INFO[nIndex]);
@@ -39,7 +43,7 @@ function wb_RelayCallCreateBoss(nRandSeed)
 	if npcIndex > 0 then
 		SetNpcDeathScript(npcIndex, "\\script\\function\\world_boss\\wb_boss_death.lua");
 		SetNpcRemoveScript(npcIndex, "\\script\\function\\world_boss\\wb_boss_remove.lua");
-		SetNpcLifeTime(npcIndex, 20 * 60);
+		SetNpcLifeTime(npcIndex, 40 * 60);
 	end
 end
 
@@ -112,11 +116,28 @@ end
 
 --»÷É±boss½±Àø
 function wb_KillAward()
-	gf_Modify("Exp", 8000000);
-	gf_AddItemEx2({2, 1, 30669, 20}, "M¶nh MËt TÞch", "BOSS ThÕ Giíi", "Tiªu diÖt BOSS", 0, 1)
-	local msg = format("Chóc mõng [%s] ®· hoµn thµnh ®ßn kÕt liÔu %s, nhËn ®­îc %d kinh nghiÖm, %s*%d", GetName(), wb_GetBossName() or "BOSS", 8000000, "M¶nh MËt TÞch", 20);
-	Msg2Global(msg);
-	AddLocalNews(msg);
+	if GetWorldPos() == 202 then
+		AddItem(2,1,30072,1);
+	else
+		gf_Modify("Exp", 50000000);
+		gf_AddItemEx({2, 5, 7, 4,0}, "Gi¸m ®Þnh phï 7")
+		-- gf_AddItemEx({2, 1, 1009, 50,0}, "Thiªn th¹ch tinh th¹ch")
+		-- -- gf_AddItemEx2({2,95,204, 5}, "TCL", "BOSS Th? gi¨ªi", "Tiau di?t  BOSS", 0, 1)
+		-- -- gf_AddItemEx2({2,1,30458, 2}, "Tô Linh Hoµn", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		 -- -- gf_AddItemEx2({2,1,30668, 5}, "THUL 6", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- gf_AddItemEx2({2,1,30424, 5}, "Bïa C­êng Hãa 12", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- -- gf_AddItemEx2({2,1,30425, 10}, "Bïa C­êng Hãa 13", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- -- gf_AddItemEx2({2,1,30426, 20}, "Bïa C­êng Hãa 14", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- -- gf_AddItemEx2({2,1,30499, 20}, "HCAH", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- -- gf_AddItemEx2({2,1,30402, 30}, "Ngò Th¸i Th¸nh Háa", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- local nRetCode, nIndex = gf_AddItemEx2({2,1,50008, 1}, "Trïm diÖt Boss hiÖu", "BOSS ThÕ gíi", "Tiªu diÖt BOSS ThÕ giíi", 0, 1)
+		-- if nRetCode == 1 then
+				-- SetItemExpireTime(nIndex,24 * 3600);
+		-- end
+		-- local msg = format("Chóc mõng [%s] ®· hoµn thµnh ®ßn kÕt liÔu %s, nhËn ®­îc %d kinh nghiÖm, %s*%d, %s", GetName(), wb_GetBossName() or "BOSS", 10000000, "Bïa C­êng Hãa", 10, "Trïm diÖt Boss hiÖu");
+		-- Msg2Global(msg);
+		-- AddLocalNews(msg);
+	end
 end
 
 function wb_SmallBoxAward()
@@ -124,20 +145,52 @@ function wb_SmallBoxAward()
 		return 0;
 	end
 	--½±Àø
-	gf_Modify("Exp", 4000000);
+	gf_Modify("Exp", 14000000);
 	local tAward = {
-		{1, 50000, "M¶nh MËt TÞch", {2, 1, 30669, 1, 4}, 0},
-		--{1, 0, "¸ß¼¶Ê¦ÃÅÃØ¼®°ü¹ü", {2, 1, 30627, 1, 4}, 0},
-		--{1, 0, "Ê¦ÃÅÕæ¾í°ü¹ü", {2, 1, 30665, 1, 4}, 0},
-		--{1, 0, "ÐÞÕæÒª¾÷²ÐÒ³", {2, 1, 30315, 1, 4}, 0},
-		{1, 9999, "§¹i Nh©n s©m", {2, 0, 553, 1, 4}, 0},
-		{1, 15000, "TiÓu Nh©n s©m qu¶", {2, 0, 552, 1, 4}, 0},
-		{1, 25000, "Tu Ch©n YÕu QuyÕt", {2, 0, 554, 1, 4}, 0},
-		{1, 1, "MËt TÞch Tiªu Dao", {0, 112, 224, 1}, 0, 0, 0, 1},
+			{1, 50000, "Gi¸m ®Þnh phï 7", {2, 5, 7, 4, 4}, 0},
+			{1, 10000, "Ngò th¸i long tóy", {2, 1, 3204, 1, 0}, 0},
+			{1, 50000, "Tam DiÖu Long Hi", {2, 1, 3331, 5, 0}, 0},
+		-- --{1, 0, "¸ß¼¶Ê¦ÃÅÃØ¼®°ü¹ü", {2, 1, 30627, 1, 4}, 0},
+		-- --{1, 0, "Ê¦ÃÅÕæ¾í°ü¹ü", {2, 1, 30665, 1, 4}, 0},
+		-- --{1, 0, "ÐÞÕæÒª¾÷²ÐÒ³", {2, 1, 30315, 1, 4}, 0},
+		-- {1, 9999, "§¹i Nh©n s©m", {2, 0, 553, 1, 4}, 0},
+		-- {1, 15000, "TiÓu Nh©n s©m qu?", {2, 0, 552, 1, 4}, 0},
+		-- {1, 25000, "m?ng thian cang", {2, 1, 30390, 500, 4}, 0},
+		-- {1, 1, "MËt TÞch Tiªu Dao", {2, 1, 30410, 100}, 0, 0, 0, 1},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 4, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 8, 1, 4}, 0},
+			-- {1, 3000, "S¸ch pet 4", {2, 150, 12, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 16, 1, 4}, 0},
+			-- {1, 3000, "S¸ch pet 4", {2, 150, 20, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 24, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 28, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 32, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 36, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 40, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 44, 1, 4}, 0},
+			-- {1, 3000, "S¸ch pet 4", {2, 150, 48, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 52, 1, 4}, 0},
+			-- {1, 3000, "S¸ch pet 4", {2, 150, 56, 1, 4}, 0},
+			-- {1, 25000, "S¸ch pet 4", {2, 150, 60, 1, 4}, 0},
+			
 	}
-	gf_EventGiveRandAward(tAward, gf_SumRandBase(tAward), 1, "BOSS ThÕ Giíi", "§¹i ChiÕn B¶o H¹p")
+	gf_EventGiveRandAward(tAward, gf_SumRandBase(tAward), 1, "BOSS ThÕ giíi", "§¹i ChiÕn B¶o H¹p")
+	-- gf_AddItemEx2({2,1,30668, 2}, "THUL 6", "BOSS ThÕ gíi", "§¹i ChiÕn B¶o H¹p", 0, 1);
+	-- gf_AddItemEx2({2,1,30499, 30}, "HCAH", "BOSS ThÕ gíi", "§¹i ChiÕn B¶o H¹p", 0, 1);
+	-- gf_AddItemEx2({2,1,30458, 1}, "Tô Linh Hoµn", "BOSS ThÕ gíi", "§¹i ChiÕn B¶o H¹p", 0, 1);
+	-- gf_AddItemEx2({2,1,30402, 30}, "Ng÷ Th¸i Th¸nh Háa", "BOSS ThÕ gíi", "§¹i ChiÕn B¶o H¹p", 0, 1);
+	--»î¶
+	local rand =random(1,3)
+	-- if rand == 1 then
+		-- UL3_non();
+	-- end
+	-- if rand == 2 then
+		-- UL3_ao();
+	-- end
+	-- if rand == 3 then
+		-- UL3_quan();
+	-- end
 	
-	--»î¶¯
 	vaf_get_prize_item_30709(5, 1);
 	AddRuntimeStat(23, 2, 0, 1);
 	return 1;
@@ -153,11 +206,11 @@ function wb_BigBoxAward()
 		Msg2Player("§éng t¸c qu¸ chËm, b¶o r­¬ng ®· bÞ ng­êi kh¸c nhÆt mÊt");
 		return 0;
 	end
-	local nMapId = GetWorldPos();
-	if nMapId ~= wb_GetBossMapId() then
-		Talk(1,"","B¶n ®å hiÖn t¹i kh«ng thÓ më b¶o r­¬ng")
-		return 0;
-	end
+--	local nMapId = GetWorldPos();
+--	if nMapId ~= wb_GetBossMapId() then
+--		Talk(1,"","B¶n ®å hiÖn t¹i kh«ng th?m?b¶o r­¬ng")
+--		return 0;
+--	end
 	if IsPlayerDeath() == 1 then
 		return 0;
 	end
@@ -167,21 +220,40 @@ function wb_BigBoxAward()
 	SetNpcLifeTime(npcIndex, 0);
 	wb_SetBigBoxFlag();
 	--½±Àø
-	gf_Modify("Exp", 10000000);
-	gf_AddItemEx2({2, 1, 30669, 40}, "M¶nh MËt TÞch", "BOSS ThÕ Giíi", "R­¬ng Vµng §¹i ChiÕn", 0, 1);
-	local tAward = {
-		{1, 14, "L¨ng Ba Vi Bé", {0, 112, 78, 1}, 0},
-		--{1, 0, "Ê¦ÃÅÕæ¾í°ü¹ü", {2, 1, 30665, 1}, 0},
-		{1, 15, "MËt TÞch Tiªu Dao Tµn QuyÓn", {0, 112, 225, 1}, 0},
-		{1, 50, "Tu Ch©n YÕu QuyÕt", {2, 0, 554, 3}, 0},
-		{1, 10, "§¹i Nh©n s©m", {2, 0, 553, 1}, 0},
-		{1, 10, "TiÓu Nh©n s©m qu¶", {2, 0, 552, 2}, 0},		
-		{1, 1, "MËt TÞch Tiªu Dao", {0, 112, 224, 1}, 0, 0, 0, 1},
-	}
-	gf_EventGiveRandAward(tAward, gf_SumRandBase(tAward), 1, "BOSS ThÕ Giíi", "R­¬ng Vµng §¹i ChiÕn");	
-	
+	gf_Modify("Exp", 5000000);
+	gf_AddItemEx({2, 1, 1009, random(50,60),0}, "Thiªn th¹ch tinh th¹ch")
+--	gf_AddItemEx2({2, 1, 30669, 40}, "M¶nh MËt TÞch", "BOSS ThÕ gíi", "R­¬ng Vµng §¹i ChiÕn", 0, 1);
+	-- local tAward = {
+		-- -- {1, 14, "L¨ng Ba Vi B?", {0, 112, 78, 1}, 0},
+		-- -- --{1, 0, "Ê¦ÃÅÕæ¾í°ü¹ü", {2, 1, 30665, 1}, 0},
+		-- -- {1, 15, "MËt TÞch Tiªu Dao Tµn QuyÓn", {0, 112, 225, 1}, 0},
+		-- -- {1, 50, "Tu Ch©n YÕu QuyÕt", {2, 0, 554, 3}, 0},
+		-- {1, 10, "Bïa C­êng Hãa  14", {2,1,30426, 30}, 0},
+		-- {1, 10, "Bïa C­êng Hãa 14", {2,1,30426, 30}, 0},		
+		-- {1, 10, "Bïa C­êng Hãa 14", {2,1,30426, 30}, 0, 0, 0, 1},
+	-- }
+	-- gf_EventGiveRandAward(tAward, gf_SumRandBase(tAward), 1, "BOSS Th? gi¨ªi", "R-?ng V¦Ìng ¡ì1i Chi?n");	
+	 -- gf_AddItemEx2({2,1,30668, 5}, "THUL 6", "BOSS ThÕ giíi", "Tiªu diÖt  BOSS", 0, 1);
+	 -- gf_AddItemEx2({2,1,30427, 10}, "Bïa C­êng Hãa 12", "BOSS ThÕ giíi", "Tiªu diÖt  BOSS", 0, 1);
+	 Earn(5000000);
+	 local msg = format("Chóc mõng [%s] ®· giµnh ®­îc r­¬ng Vµng §¹i ChiÕn, thu ®­îc 1 sè Thiªn th¹ch tinh th¹ch",GetName());
+	Msg2Global(msg);
+	AddLocalNews(msg);
 	--»î¶¯
 	vaf_get_prize_item_30709(10, 1);
 	AddRuntimeStat(23, 3, 0, 1);
 	return 1;
+end
+
+function UL3_non()
+	local non = random(35,51)
+	-- AddItem(0, 148, non, 1, 1);
+end
+function UL3_ao()
+	local non = random(35,51)
+	-- AddItem(0, 149, non, 1, 1);
+end
+function UL3_quan()
+	local non = random(35,51)
+	-- AddItem(0, 150, non, 1, 1);
 end
