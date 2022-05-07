0504beca9b6c941f2af81334d69648fb4428a795
duyvo150896 <79331822+duyvo150896@users.noreply.github.com>
2022-05-07 17:03:41 +0700

    tu quang lenh

--- script/missions/tong_mission/item/item_ziguangling.lua
+++ script/missions/tong_mission/item/item_ziguangling.lua
@@ -5,8 +5,8 @@ Include("\\script\\missions\\tong_mission\\main_head.lua");
 g_szItemName = "Tö Quang LÖnh";
 function OnUse(nItemIdx)
 	if DelItemByIndex(nItemIdx,1) == 1 then
-		SetTask(TSK_POINT,GetTask(TSK_POINT)+100);
-		Msg2Player("B¹n ®· thu ®­îc 100 ®iÓm Tö Quang C¸c");
+		SetTask(TSK_POINT,GetTask(TSK_POINT)+100000);
+		Msg2Player("B¹n ®· thu ®­îc 100000 ®iÓm Tö Quang C¸c");
 		WriteLog("[Bang héi v­ît ¶i]:"..GetName().."Sö dông 1	"..g_szItemName);
 	end;
 end;
